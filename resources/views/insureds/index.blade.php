554ba4030b9666bec3f001cf4398794757568d14
nomi3 <thisisnomi3@gmail.com>
2024-04-30 07:28:03 +0900

    必要なデータを全て表示するように変更

--- resources/views/insureds/index.blade.php
+++ resources/views/insureds/index.blade.php
@@ -27,6 +27,42 @@
                                 <th>メールアドレス</th>
                                 <th>保険証番号</th>
                                 <th>保険証記号</th>
+                                <th>委託元保険者</th>
+                                <th>所属保険者</th>
+                                <th>保険者番号</th>
+                                <th>支援レベル</th>
+                                <th>性別</th>
+                                <th>生年月日</th>
+                                <th>年齢</th>
+                                <th>健診日</th>
+                                <th>身長</th>
+                                <th>健診時体重</th>
+                                <th>BMI</th>
+                                <th>健診時腹囲</th>
+                                <th>収縮期1</th>
+                                <th>収縮期2</th>
+                                <th>収縮期その他</th>
+                                <th>拡張期1</th>
+                                <th>拡張期2</th>
+                                <th>拡張期その他</th>
+                                <th>中性脂肪</th>
+                                <th>空腹時中性脂肪</th>
+                                <th>随時中性脂肪</th>
+                                <th>HDL</th>
+                                <th>LDL</th>
+                                <th>GOT</th>
+                                <th>GPT</th>
+                                <th>γ-GT</th>
+                                <th>空腹時血糖</th>
+                                <th>随時血糖</th>
+                                <th>HBA1C</th>
+                                <th>服薬1</th>
+                                <th>服薬2</th>
+                                <th>服薬3</th>
+                                <th>喫煙</th>
+                                <th>初回面談日</th>
+                                <th>初回面談時間</th>
+                                <th>対象者の特徴</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -38,6 +74,42 @@
                                     <td>{{ $insured->email }}</td>
                                     <td>{{ $insured->insurance_card_number }}</td>
                                     <td>{{ $insured->insurance_card_symbol }}</td>
+                                    <td>{{ $insured->principal_insurer }}</td>
+                                    <td>{{ $insured->affiliated_insurer }}</td>
+                                    <td>{{ $insured->insurer_number }}</td>
+                                    <td>{{ $insured->support_level }}</td>
+                                    <td>{{ $insured->gender }}</td>
+                                    <td>{{ $insured->birth_date }}</td>
+                                    <td>{{ $insured->age }}</td>
+                                    <td>{{ $insured->checkup_date }}</td>
+                                    <td>{{ $insured->height }}</td>
+                                    <td>{{ $insured->weight }}</td>
+                                    <td>{{ $insured->bmi }}</td>
+                                    <td>{{ $insured->waist }}</td>
+                                    <td>{{ $insured->systolic1 }}</td>
+                                    <td>{{ $insured->systolic2 }}</td>
+                                    <td>{{ $insured->systolic_other }}</td>
+                                    <td>{{ $insured->diastolic1 }}</td>
+                                    <td>{{ $insured->diastolic2 }}</td>
+                                    <td>{{ $insured->diastolic_other }}</td>
+                                    <td>{{ $insured->triglycerides }}</td>
+                                    <td>{{ $insured->fasting_triglycerides }}</td>
+                                    <td>{{ $insured->casual_triglycerides }}</td>
+                                    <td>{{ $insured->hdl_cholesterol }}</td>
+                                    <td>{{ $insured->ldl_cholesterol }}</td>
+                                    <td>{{ $insured->got }}</td>
+                                    <td>{{ $insured->gpt }}</td>
+                                    <td>{{ $insured->gamma_gt }}</td>
+                                    <td>{{ $insured->fasting_glucose }}</td>
+                                    <td>{{ $insured->casual_glucose }}</td>
+                                    <td>{{ $insured->hba1c }}</td>
+                                    <td>{{ $insured->medication1 }}</td>
+                                    <td>{{ $insured->medication2 }}</td>
+                                    <td>{{ $insured->medication3 }}</td>
+                                    <td>{{ $insured->smoking }}</td>
+                                    <td>{{ $insured->initial_interview_date }}</td>
+                                    <td>{{ $insured->initial_interview_time }}</td>
+                                    <td>{{ $insured->characteristics }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
@@ -62,7 +134,43 @@
                                         <td>${item.email}</td>
                                         <td>${item.insurance_card_number}</td>
                                         <td>${item.insurance_card_symbol}</td>
-                                    </tr>`;
+                                        <td>${item.principal_insurer}</td>
+                                        <td>${item.affiliated_insurer}</td>
+                                        <td>${item.insurer_number}</td>
+                                        <td>${item.support_level}</td>
+                                        <td>${item.gender}</td>
+                                        <td>${item.birth_date}</td>
+                                        <td>${item.age}</td>
+                                        <td>${item.checkup_date}</td>
+                                        <td>${item.height}</td>
+                                        <td>${item.weight}</td>
+                                        <td>${item.bmi}</td>
+                                        <td>${item.waist}</td>
+                                        <td>${item.systolic1}</td>
+                                        <td>${item.systolic2}</td>
+                                        <td>${item.systolic_other}</td>
+                                        <td>${item.diastolic1}</td>
+                                        <td>${item.diastolic2}</td>
+                                        <td>${item.diastolic_other}</td>
+                                        <td>${item.triglycerides}</td>
+                                        <td>${item.fasting_triglycerides}</td>
+                                        <td>${item.casual_triglycerides}</td>
+                                        <td>${item.hdl_cholesterol}</td>
+                                        <td>${item.ldl_cholesterol}</td>
+                                        <td>${item.got}</td>
+                                        <td>${item.gpt}</td>
+                                        <td>${item.gamma_gt}</td>
+                                        <td>${item.fasting_glucose}</td>
+                                        <td>${item.casual_glucose}</td>
+                                        <td>${item.hba1c}</td>
+                                        <td>${item.medication1}</td>
+                                        <td>${item.medication2}</td>
+                                        <td>${item.medication3}</td>
+                                        <td>${item.smoking}</td>
+                                        <td>${item.initial_interview_date}</td>
+                                        <td>${item.initial_interview_time}</td>
+                                        <td>${item.characteristics}</td>
+                                        </tr>`;
                                     tableBody.innerHTML += row;
                                 });
                             })
