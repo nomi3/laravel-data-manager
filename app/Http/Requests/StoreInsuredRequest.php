4b9e2f93f08ab99ea2d42e5190b39480acbdb06e
nomi3 <thisisnomi3@gmail.com>
2024-04-29 18:59:47 +0900

    csvファイルのみを受けるようにバリデーションを追加

--- app/Http/Requests/StoreInsuredRequest.php
+++ app/Http/Requests/StoreInsuredRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreInsuredRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreInsuredRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
@@ -22,7 +23,7 @@ class StoreInsuredRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'csv_file' => 'required|file|mimes:csv',
         ];
     }
 }
