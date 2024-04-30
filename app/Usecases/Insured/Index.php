a84ec90453d3917ee0129e63632a98104fd4b308
nomi3 <thisisnomi3@gmail.com>
2024-04-30 19:47:09 +0900

    一覧取得のusecase化

---
+++ app/Usecases/Insured/Index.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Usecases\Insured;
+
+use App\Models\Insured;
+use Illuminate\Database\Eloquent\Collection;
+
+class Index
+{
+    public function __invoke(): Collection
+    {
+        $insureds = Insured::all();
+
+        return $insureds;
+    }
+}
